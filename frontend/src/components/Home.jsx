a39d975b4c9e9b884356a92e7b2d6d03e618736c
Ishita Datta <ishitadatta288@gmail.com>
2025-07-04 13:35:48 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../public/logo.png";
-import { Link, Links } from "react-router-dom";
-import { FaFacebook, FaTruck } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
 import axios from "axios";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import toast from "react-hot-toast";
 import { BACKEND_URL } from "../utils/utils.js";
-
 function Home() {
   const [courses, setCourses] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // token
   useEffect(() => {
     const user = localStorage.getItem("user");
     if (user) {
@@ -24,19 +24,7 @@ function Home() {
     }
   }, []);
 
-  const handleLogout = async () => {
-    try {
-      const response = await axios.get(`${BACKEND_URL}/user/logout`, {
-        withCredentials: true,
-      });
-      toast.success(response.data.message);
-      localStorage.removeItem("user");
-      setIsLoggedIn(false);
-    } catch (error) {
-      console.log("Error in logging out", error);
-      toast.error(error.response.data.error || "Error in logging out");
-    }
-  };
+  // fetch courses
   useEffect(() => {
     const fetchCourses = async () => {
       try {
@@ -46,11 +34,26 @@ function Home() {
         console.log(response.data.courses);
         setCourses(response.data.courses);
       } catch (error) {
-        console.log("Error in fetchCourses", error);
+        console.log("error in fetchCourses ", error);
       }
     };
     fetchCourses();
   }, []);
+
+  // logout
+  const handleLogout = async () => {
+    try {
+      const response = await axios.get(`${BACKEND_URL}/user/logout`, {
+        withCredentials: true,
+      });
+      toast.success(response.data.message);
+      localStorage.removeItem("user");
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.log("Error in logging out ", error);
+      toast.error(error.response.data.errors || "Error in logging out");
+    }
+  };
 
   var settings = {
     dots: true,
@@ -89,19 +92,25 @@ function Home() {
   };
 
   return (
-    <div className=" min-h-screen w-full bg-gradient-to-r from-black to-blue-950">
-      <div className=" text-white container mx-auto">
-        {/* header */}
-        <header className=" flex items-center justify-between p-8">
-          <div className=" flex items-center space-x-2">
-            <img className=" h-10 w-10 rounded-full" src={logo} alt="" />
-            <h1 className="text-2xl text-orange-500 font-bold">Edulocity</h1>
+    <div className="bg-gradient-to-r from-black to-blue-950 ">
+      <div className="h-[1250px] md:h-[1050px] text-white container mx-auto">
+        {/* Header */}
+        <header className="flex items-center justify-between p-6 ">
+          <div className="flex items-center space-x-2">
+            <img
+              src={logo}
+              alt=""
+              className="w-7 h-7 md:w-10 md:h-10 rounded-full"
+            />
+            <h1 className="md:text-2xl text-orange-500 font-bold">
+              CourseHaven
+            </h1>
           </div>
-          <div className=" space-x-4">
+          <div className="space-x-4">
             {isLoggedIn ? (
               <button
                 onClick={handleLogout}
-                className=" bg-transparent text-white py-2 px-4 border border-white rounded"
+                className="bg-transparent text-white text-xs md:text-lg md:py-2 md:px-4 p-2 border border-white rounded"
               >
                 Logout
               </button>
@@ -124,51 +133,49 @@ function Home() {
           </div>
         </header>
 
-        {/* main section */}
-        <section className=" text-center py-20">
-          <h1 className=" text-4xl font-semibold text-orange-500">Edulocity</h1>
+        {/* Main section */}
+        <section className="text-center py-20">
+          <h1 className="text-4xl font-semibold text-orange-500">
+            CourseHaven
+          </h1>
 
           <br />
-          <p className=" text-gray-500 ">
-            {" "}
-            Sharpen your skills with vourses crafted by experts.
+          <p className="text-gray-500">
+            Sharpen your skills with courses crafted by experts.
           </p>
-          <div className=" space-x-4 mt-8">
+          <div className="space-x-4 mt-8">
             <Link
               to={"/courses"}
-              className=" bg-green-500 text-white rounded py-3 px-6 font-semibold hover:bg-white duration-300 hover:text-black"
+              className="bg-green-500 text-white p-2 md:py-3 md:px-6 rounded font-semibold hover:bg-white duration-300 hover:text-black"
             >
               Explore courses
             </Link>
             <Link
-              to={
-                "https://www.coursera.org/?msockid=1db8640ee6846666353d7648e7366750"
-              }
-              className=" bg-white text-black rounded py-3 px-6 font-semibold hover:bg-green-500 duration-300 hover:text-white"
+              to={"https://www.youtube.com/learncodingofficial"}
+              className="bg-white text-black  p-2 md:py-3 md:px-6 rounded font-semibold hover:bg-green-500 duration-300 hover:text-white"
             >
-              Course Videos
+              Courses videos
             </Link>
           </div>
         </section>
-
-        <section>
-          <Slider {...settings}>
+        <section className="p-10">
+          <Slider className="" {...settings}>
             {courses.map((course) => (
-              <div key={course._id} className=" p-4">
-                <div className=" relative flex-shrink-0 w-92 transition-transform duration-300 transform hover:scale-105">
-                  <div className=" bg-gray-900 rounded-lg overflow-hidden">
+              <div key={course._id} className="p-4">
+                <div className="relative flex-shrink-0 w-92 transition-transform duration-300 transform hover:scale-105">
+                  <div className="bg-gray-900 rounded-lg overflow-hidden">
                     <img
-                      className=" h-36 w-full object-contain"
-                      src={course.image?.url}
+                      className="h-32 w-full object-contain"
+                      src={course.image.url}
                       alt=""
                     />
-                    <div className=" p-10 text-center">
-                      <h2 className=" text-xl font-bold text-white">
+                    <div className="p-6 text-center">
+                      <h2 className="text-xl font-bold text-white">
                         {course.title}
                       </h2>
                       <Link
-                        to={"/courses"}
-                        className=" inline-block mt-8 bg-orange-500 text-white py-2 px-5 rounded-full hover:bg-blue-500 duration-300"
+                        to={`/buy/${course._id}`}
+                        className="mt-8 bg-orange-500 text-white py-2 px-4 rounded-full hover:bg-blue-500 duration-300"
                       >
                         Enroll Now
                       </Link>
@@ -180,62 +187,59 @@ function Home() {
           </Slider>
         </section>
 
-        <hr className=" my-10" />
+        <hr />
         {/* Footer */}
-
-        <footer className="">
-          <div className=" grid grid-cols-1 md:grid-cols-3">
-            <div className=" flex flex-col items-center md:items-start">
-              <div className=" flex items-center space-x-2">
-                <img className=" h-10 w-10 rounded-full" src={logo} alt="" />
+        <footer className="my-12">
+          <div className="grid grid-cols-1 md:grid-cols-3">
+            <div className="flex flex-col items-center md:items-start">
+              <div className="flex items-center space-x-2">
+                <img src={logo} alt="" className="w-10 h-10 rounded-full" />
                 <h1 className="text-2xl text-orange-500 font-bold">
-                  Edulocity
+                  CourseHaven
                 </h1>
               </div>
-              <div className=" mt-3 ml-2 md:ml-8 ">
-                <p className=" mb-2">Follow us</p>
-                <div className=" flex space-x-4">
+              <div className="mt-3 ml-2 md:ml-8">
+                <p className="mb-2">Follow us</p>
+                <div className="flex space-x-4">
                   <a href="">
-                    <FaFacebook className=" text-2xl hover:text-blue-500" />
+                    <FaFacebook className="text-2xl hover:text-blue-400 duration-300" />
                   </a>
                   <a href="">
-                    <FaInstagram className=" text-2xl hover:text-pink-600" />
+                    <FaInstagram className="text-2xl hover:text-pink-600 duration-300" />
                   </a>
                   <a href="">
-                    <FaXTwitter className="text-2xl hover:text-blue-400" />
+                    <FaTwitter className="text-2xl hover:text-blue-600 duration-300" />
                   </a>
                 </div>
               </div>
             </div>
 
-            <div className=" items-center flex flex-col">
-              <h3 className=" text-lg font-semibold mb-4"> Connects</h3>
-              <ul className="space-y-2 text-gray-400">
-                <li className=" hover:text-white cursor-pointer duration-300">
-                  Youtube
+            <div className="items-center mt-6 md:mt-0 flex flex-col">
+              <h3 className="text-lg font-semibold md:mb-4">connects</h3>
+              <ul className=" space-y-2 text-gray-400">
+                <li className="hover:text-white cursor-pointer duration-300">
+                  youtube- learn coding
                 </li>
-                <li className=" hover:text-white cursor-pointer duration-300">
-                  Telegram
+                <li className="hover:text-white cursor-pointer duration-300">
+                  telegram- learn coding
                 </li>
-                <li className=" hover:text-white cursor-pointer duration-300">
-                  Github
+                <li className="hover:text-white cursor-pointer duration-300">
+                  Github- learn coding
                 </li>
               </ul>
             </div>
-            <div className=" items-center flex flex-col">
-              <h3 className=" text-lg font-semibold mb-4 px-2">
-                {" "}
-                Copyrights &#169; 2025
+            <div className="items-center mt-6 md:mt-0 flex flex-col">
+              <h3 className="text-lg font-semibold mb-4">
+                copyrights &#169; 2024
               </h3>
-              <ul className=" space-y-2 text-gray-400">
-                <li className=" hover:text-white cursor-pointer duration-300">
+              <ul className=" space-y-2 text-center text-gray-400">
+                <li className="hover:text-white cursor-pointer duration-300">
                   Terms & Conditions
                 </li>
-                <li className=" hover:text-white cursor-pointer duration-300">
+                <li className="hover:text-white cursor-pointer duration-300">
                   Privacy Policy
                 </li>
-                <li className=" hover:text-white cursor-pointer duration-300">
-                  {" "}
+                <li className="hover:text-white cursor-pointer duration-300">
                   Refund & Cancellation
                 </li>
               </ul>
